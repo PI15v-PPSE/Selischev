bec8ed051fcbfd9f6d3a3e8a454507c265954b50
slschv <slonuk111@gmail.com>
2018-06-10 10:35:19 +0300

    add js code

---
+++ js/prog.js
@@ -0,0 +1,18 @@
+function SeaBattle(targetContainer, edge) {
+    //Список css-классов
+    var cssClasses = {
+        container: 'container',
+        block: 'block',
+        area: 'area',
+        seaContainer: 'seaContainer',
+        sea: 'sea',
+        messagesLog: 'messages',
+        message: 'message',
+        cell: 'cell',
+        cellDead: 'dead',
+        cellBoat: 'boat',
+        cellWater: 'water',
+        cellMiss: 'miss'
+    };
+	
+}
