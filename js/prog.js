0e21df4d84c5859539a28aa63b16675fd2a5533f
slschv <slonuk111@gmail.com>
2018-06-10 15:42:37 +0300

    +code

--- js/prog.js
+++ js/prog.js
@@ -15,4 +15,91 @@ function SeaBattle(targetContainer, edge) {
         cellMiss: 'miss'
     };
 	
+    //Список идентификаторов
+    var elementsIds = {
+        gameContainer: 'game_container',
+        playerName: 'player_name',
+        computerName: 'computer_name',
+        playerSeaContainer: 'player_field',
+        playerSea: 'player_sea',
+        computerSeaContainer: 'computer_field',
+        computerSea: 'computer_sea',
+        messagesLog: 'messages',
+        cellIdPrefix: 'cell_',
+        rowIdPrefix: 'row_'
+    };
+	
+	//Список игроков
+    var players = {
+        Player: 'player',
+        Computer: 'computer'
+    };
+
+    //Статусы ячеек на карте
+    var cellType = {
+        dead: 1,
+        ship: 0,
+        water: -1,
+        miss: 2
+    };
+	
+    //Завершена ли игра
+    var gameEnded = false;
+
+    //Поле с сообщениями
+    var messagesArea;
+
+    //Карты полей боя игроков
+    var computerMap, playerMap;
+
+    //Лог выстрелов компьютера
+    var computerShots;
+
+    //Имена игроков
+    var playerName, computerName;
+
+    //Список кораблей и их координат у игроков
+    var playerShips, computerShips;
+
+    //Список сторон, на которых можно расположить корабль
+    var possibleDirections = ['top', 'bottom', 'right', 'left'];
+
+    //Получаем html-текст поля ввода с именем игрока
+    var getPlayerNameInputHtml = function (id, defaultName, title, placeholder, autofocus) {
+        return $('<input>')
+            .attr('id', id)
+            .attr('type', 'text')
+            .attr('placeholder', placeholder)
+            .attr('title', title)
+            .attr('autofocus', autofocus ? 'true' : 'false')
+            .attr('value', defaultName)
+            .prop('outerHTML');
+    };
+	
+    //Получаем игровое поле для указанного игрока
+    var getSeaArea = function (id, labelValue, inputId, inputDefaultValue, inputTitle, inputPlaceholder, inputAutoFocus) {
+        return $('<div>')
+            .attr('class', cssClasses.block)
+            .append(
+                $('<label>')
+                    .attr('for', id)
+                    .html(
+                        concat(
+                            labelValue,
+                            getPlayerNameInputHtml(
+                                inputId,
+                                inputDefaultValue,
+                                inputTitle,
+                                inputPlaceholder,
+                                inputAutoFocus)
+                        )
+                    )
+            )
+            .append(
+                $('<div>')
+                    .attr('id', id)
+                    .attr('class', concat(cssClasses.area, cssClasses.seaContainer))
+            );
+    };
+
 }
